69bc86a56afee5ef1c230a5748b14d7efdd31e44
drpinio <jakub.pinczewski@vp.pl>
2022-09-14 15:40:26 +0200

    Komentarz

--- LEKCJA1/1.php
+++ LEKCJA1/1.php
@@ -5,10 +5,16 @@
 <body>
 <?php
 $a=1;
+$b=0 ;
+
 echo "Hellp World<br>";
 echo "my counter $a <br>";
-echo "Hello friend";
-
+echo "Hello friend<br>";
+echo "Mamy $a lekcje<br>";
+if($b == 0)
+{echo "Nie masz wiadomości<br>";}
+else 
+ {echo "Masz $b wiadomości";} ;
 ?>
 </body>
 </html>
