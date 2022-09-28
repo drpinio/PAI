78113ffe9b861cafbf6d8cf6c9c40aca7b70f730
drpinio <jakub.pinczewski@vp.pl>
2022-09-28 14:55:02 +0200

    k

---
+++ l5/2.php
@@ -0,0 +1,25 @@
+<?php
+    $chessboard = array(
+        array('w','s','g','h','k','g','s','w'),
+        array('p','p','p','p','p','p','p','p'),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array('P','P','P','P','P','P','P','P'),
+        array('W','S','G','H','K','G','S','W'),
+    );
+	
+	  $chessboard[6][3]=" ";
+	  $chessboard[5][3]="P";
+	
+	
+    echo "<pre>";
+    foreach($chessboard as $row) {
+        foreach($row as $piece) 
+            echo "$piece ";
+		
+        echo "<br>";
+    }
+    echo "</pre>";
+?>
